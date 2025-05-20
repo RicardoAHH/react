cb57e3afb864f854924cd1fca1bc52372f2fb585
Ricardo Hernandez <richhquim@gmail.com>
2025-05-19 20:45:22 -0600

    m

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/Button.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,8 +31,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
       <h1>Hola mundo</h1>
-      <p className='text-red-600'>Color rojo</p>
+      <p className='text-red-600 font-bold'>Color rojo</p>
     </>
+
   )
 }
 
